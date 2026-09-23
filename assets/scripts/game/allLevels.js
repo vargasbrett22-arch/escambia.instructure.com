5af8defe38d60d05cb842809fd4bdb0548b51af9
vargasbrett22-arch <vargasbrett22@gmail.com>
2026-09-22 20:14:11 -0500

    Update allLevels.js

--- assets/scripts/game/allLevels.js
+++ assets/scripts/game/allLevels.js
@@ -1,4 +1,4 @@
-window.allLevels = [
+[
     [
         "stereo_madness",
         "Stereo Madness",
@@ -138,9 +138,9 @@ window.allLevels = [
         ["Jax", "Step"]
     ],
     [
-        "jackson_web_dashers",
-        "Jackson Web Dashers",
-        "level_565",
+        "bretts_stalemate",
+        "Bretts Stalemate",
+        "level_3434",
         ["sirmircowave", "MindCap"]
     ],
     [
@@ -161,50 +161,4 @@ window.allLevels = [
         "level_99",
         ["MindCap", "Dimrain47"]
     ]
-
-    /* Disabled Levels Below 
-    ,
-    [
-        "dash",
-        "Dash",
-        "level_22",
-        ["RobTop", "MDK"]
-    ],
-    [
-        "bloodbath",
-        "Bloodbath",
-        "level_100",
-        ["Riot", "Dimrain47"]
-    ],
-    [
-        "override",
-        "Override",
-        "level_137409445",
-        ["Nightcat", "DJ-Nate"]
-    ],
-    [
-        "disco_dinosaur",
-        "Disco Dinosaur",
-        "level_137677336",
-        ["kmr", "DJ-Nate"]
-    ],
-    [
-        "the_dark_star",
-        "The Dark Star",
-        "level_116489424",
-        ["Torrenware", "MidiGuyDP"]
-    ],
-    [
-        "sunshine",
-        "Sunshine",
-        "level_56199846",
-        ["Unzor", "DjHoneyb"]
-    ],
-    [
-        "stereo_madness",
-        "Test Level",
-        "level_23",
-        ["rohanis0000", "Forever Bound"]
-    ]
-    */
-];
+]
