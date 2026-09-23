642393ad8955e17e4ccf34472c8f1b38d53cc8e8
vargasbrett22-arch <vargasbrett22@gmail.com>
2026-09-22 20:20:38 -0500

    Update allLevels.js

--- assets/scripts/game/allLevels.js
+++ assets/scripts/game/allLevels.js
@@ -1,4 +1,4 @@
-[
+window.allLevels = [
     [
         "stereo_madness",
         "Stereo Madness",
@@ -160,5 +160,48 @@
         "Every End",
         "level_99",
         ["MindCap", "Dimrain47"]
+    ],/*
+    [
+        "dash",
+        "Dash",
+        "level_22",
+        ["RobTop", "MDK"]
+    ],
+    [
+        "bloodbath",
+        "Bloodbath",
+        "level_100",
+        ["Riot", "Dimrain47"]
+    ],
+    [
+        "override",
+        "Override",
+        "level_137409445",
+        ["Nightcat", "DJ-Nate"]
+    ],
+    [
+        "disco_dinosaur",
+        "Disco Dinosaur",
+        "level_137677336",
+        ["kmr", "DJ-Nate"]
+    ],
+    [
+        "the_dark_star",
+        "The Dark Star",
+        "level_116489424",
+        ["Torrenware", "MidiGuyDP"]
+    ],
+    [
+        "sunshine",
+        "Sunshine",
+        "level_56199846",
+        ["Unzor", "DjHoneyb"]
+    ],
+    [
+        "stereo_madness",
+        "Test Level",
+        "level_23",
+        ["rohanis0000", "Forever Bound"]
     ]
-]
+    */
+];
