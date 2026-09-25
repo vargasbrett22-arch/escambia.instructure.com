d9c2973c2902dbbbcb73f982aabfc9cd1abcfc14
vargasbrett22-arch <vargasbrett22@gmail.com>
2026-09-25 18:43:38 -0500

    Update game-scene.js

--- assets/scripts/core/game-scene.js
+++ assets/scripts/core/game-scene.js
@@ -4397,6 +4397,7 @@ this._menuUpdateLogBtn = this.add.image(screenWidth - 30 - 50, 33, "GJ_WebSheet"
         "level_4284013": "diffIcon_06_btn_001",
         "level_56199846": "diffIcon_04_btn_001",
         "level_23":       "diffIcon_10_btn_001"
+        "level_3434":     "diffIcon_07_btn_001",
       };
       const diffIconKey = levelDifficultyMap[levelId] || "diffIcon_05_btn_001";
       const diffFrame = diffIconKey + ".png";
